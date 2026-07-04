ab1f5fb818787ea062af7b1b347d5d6246d75150
Sathyavasu Arunachalam <sathyavasu@Swayambu.local>
2026-07-04 13:43:17 +0530

    Update Vishnusahasranamam PWA

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "vishnusahasranamam-static-pwa-v2";
+const APP_SHELL = [
+  "./",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "manifest.webmanifest",
+  "icon.svg",
+  "data/search-data.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
